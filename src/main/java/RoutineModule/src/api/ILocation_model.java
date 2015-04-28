97726d68faec3379b840790a758c9bb1f2234b8b
מאור <maorsegal3@gmail.com>
2015-04-29 01:25:31 +0300

    מיקום חבר קהילה...

--- src/main/java/RoutineModule/src/api/ILocation_model.java
+++ src/main/java/RoutineModule/src/api/ILocation_model.java
@@ -1,7 +1,10 @@
 package RoutineModule.src.api;
 
+import java.util.HashMap;
+
 /**
  * Created by NAOR on 28/04/2015.
  */
 public interface ILocation_model {
+    public Object transferLocation(HashMap<String, String> data);
 }
--- src/main/java/RoutineModule/src/model/Location_V1.java
+++ src/main/java/RoutineModule/src/model/Location_V1.java
@@ -1,7 +1,24 @@
 package RoutineModule.src.model;
 
+import CommunicationModule.src.model.CommToUsersFactory_V1;
+import CommunicationModule.src.model.CommToUsers_V1;
+import RoutineModule.src.api.ILocation_model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by NAOR on 28/04/2015.
  */
-public class Location_V1 {
+public class Location_V1 implements ILocation_model {
+
+    public Object transferLocation(HashMap<String, String> data)
+    {
+        HashMap<Integer,HashMap<String,String>> response = new HashMap<Integer,HashMap<String,String>>();
+        response.put(1, data);
+        ArrayList<String> target = new ArrayList<String>();
+        target.add("url...");//*
+        CommToUsers_V1 comm = new CommToUsersFactory_V1().createComm(response, target, true);
+        return comm.sendResponse();
+    }
 }
