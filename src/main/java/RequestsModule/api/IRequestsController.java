64fd22d463e7bf3a10936056207e74fd9bb74af8
ohad <ohadgur@gmail.com>
2015-05-02 16:13:00 +0300

    updated pom.xml

---
+++ src/main/java/RequestsModule/api/IRequestsController.java
@@ -0,0 +1,8 @@
+package RequestsModule.api;
+
+/**
+ * Created by ohad on 27/4/2015.
+ */
+public interface IRequestsController {
+
+}
--- src/test/java/ohadTest.java
+++ src/test/java/ohadTest.java
@@ -1,9 +1,13 @@
+import java.util.HashMap;
+
 /**
  * Created by ohad on 16/4/2015.
  */
 public class ohadTest {
     public static void main(String[] args) {
-        System.out.println(new Integer(1004).toString());
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("name", "Acamol");
+        System.out.print(map.toString());
 
     }
 }
